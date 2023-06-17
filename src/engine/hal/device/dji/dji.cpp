b1dcb06cbc9c88189c2207ccf984b9ed763bd3ba
Joonas Trussmann <joonas@batman.ee>
2023-06-17 18:10:47 +0300

    Made DJI display driver work with partial, direct rendering. Moved to loop based alpha flipper.

--- src/engine/hal/device/dji/dji.cpp
+++ src/engine/hal/device/dji/dji.cpp
@@ -3,6 +3,7 @@
 #include "./dji_display.h"
 #include "./dji_services.h"
 #include "./evdev.h"
+#include <stdio.h>
 
 #define SCREEN_WIDTH 1440
 #define SCREEN_HEIGHT 810
@@ -13,32 +14,42 @@
 static dji_display_state_t *dji_display;
 static void *fb0_addr;
 static void *fb1_addr;
-static bool whichfb = 0;
 
 static lv_indev_t * keypad_indev;
-
-/*static lv_color_t buf1_1[BUFSIZE];
-static lv_color_t buf1_2[BUFSIZE];*/
-
-void dji_set_px_cb(lv_disp_drv_t * disp_drv, uint8_t * buf, lv_coord_t buf_w, lv_coord_t x, lv_coord_t y, lv_color_t color, lv_opa_t opa)
-{
-   buf += (y * SCREEN_WIDTH + x)*BYTES_PER_PIXEL;
-   lv_color_t *pixel = (lv_color_t *)buf;
-   *pixel = color;
-   buf[3] = ~opa;
-}
 
 void dji_display_flush(lv_disp_drv_t * drv, const lv_area_t * area, lv_color_t * color_p) {
   //printf("%p - %p - %p\n", fb0_addr, fb1_addr, (void*)color_p);
   //printf("x1 - %d \t x2 \t - %d \t y1 - %d \t y2 %d\n", area->x1, area->x2, area->y1, area->y2);
   //printf("w - %d \t h - %d\n", area->x2 - area->x1, area->y2 - area->y1);
-  //pthread_mutex_lock(&fb_switch_lock);
-  void* fb = (whichfb ? fb0_addr : fb1_addr);
-  //pthread_mutex_unlock(&fb_switch_lock);
+  char* fb = (char *) color_p;
+  char* otherfb = ((void*)fb == (void*)fb0_addr ? (char *)fb1_addr : (char *)fb0_addr);
+  bool whichfb = ((void*)fb == (void*)fb0_addr ? 0 : 1);
 
   if(lv_disp_flush_is_last(drv)) {
+    lv_disp_t * display = _lv_refr_get_disp_refreshing();
+    for(int a = 0; a<display->inv_p; a++) {
+      if(display->inv_area_joined[a]) {
+        continue;
+      }
+      lv_area_t invarea = display->inv_areas[a];
+      //printf("x1 - %d \t x2 \t - %d \t y1 - %d \t y2 %d\n", invarea.x1, invarea.x2, invarea.y1, invarea.y2);
+
+      for(int y = invarea.y1; y <= invarea.y2; y++) {
+        int start = (y*SCREEN_WIDTH*BYTES_PER_PIXEL)+(invarea.x1*BYTES_PER_PIXEL)+3;
+        int end = (y*SCREEN_WIDTH*BYTES_PER_PIXEL)+(invarea.x2*BYTES_PER_PIXEL)+3;
+        for(int x = start; x <= end; x=x+BYTES_PER_PIXEL) {
+          fb[x] = ~fb[x];
+          /*otherfb[x] = fb[x];
+          otherfb[x-1] = fb[x-1];
+          otherfb[x-2] = fb[x-2];
+          otherfb[x-3] = fb[x-3];*/
+        }
+        //i don't know if this is actually faster than the commented out bit above
+        //it might also depend on the invalidated regions size
+        memcpy((void*)(&otherfb[start-3]), (void*)(&fb[start-3]), end-start);
+      }
+    }
     dji_display_push_frame(dji_display, whichfb);
-    whichfb = !whichfb;
   }
 
   lv_disp_flush_ready(drv);
@@ -68,9 +79,12 @@ void hal_init() {
   disp_drv.flush_cb = dji_display_flush;
   disp_drv.hor_res = SCREEN_WIDTH;
   disp_drv.ver_res = SCREEN_HEIGHT;
-  disp_drv.set_px_cb = dji_set_px_cb;
-  disp_drv.full_refresh = 1;
-  //flush callback fails otherwise with sigsev
+  //disp_drv.set_px_cb = dji_set_px_cb;
+  disp_drv.full_refresh = 0;
+  disp_drv.direct_mode = 1;
+  //you'd think we'd want transp = 1 but it behaves real weird
+  //also if set to 1 a clear_cb is required otherwise we get a sigsev
+  //which makes sense if you read the code
   disp_drv.screen_transp = 0;
 
   lv_disp_t *disp = lv_disp_drv_register(&disp_drv);
